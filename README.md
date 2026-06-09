df9dc6966d0a48d16ca03a8e1eb870a72301287b
DKode-git <danishkn7868@gmail.com>
2026-06-09 17:39:18 +0530

    Initial project structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# TODO
+
+Project documentation coming soon.
---
+++ app/tests/test_crud.py
@@ -0,0 +1,11 @@
+"""
+Implementation pending.
+
+TODO:
+- Implement functionality.
+- Add tests.
+- Add documentation.
+"""
+
+# Implementation remaining.
+# Coming soon.
